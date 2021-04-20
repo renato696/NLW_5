f942ddb410d4fc4a1a482b9e766af6e8792419c5
Renato Pires <renato696@gmail.com>
2021-04-20 16:23:06 -0300

    Server Running with Success
    Erro no arquivo tsconfig.json corrigido.

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import express from "express";
 
 const app = express();
 
-app.listen(3333, () => console.log("Server is running on port 333"));
+app.listen(3333, () => console.log("Server is running on port 3333"));
