63c781ba17cfde821385f3da7a6fa6636cc4812e
Renato Pires <renato696@gmail.com>
2021-04-20 18:36:41 -0300

    Fim da Aula 01
    Fim da aula, criação de rotas, integração com insomnia.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,23 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
+
+/*
+* GET = busca
+* POST = criação
+* PUT = alteração
+* DELETE = deletar
+* PATCH = alterar alguma informação específica - e.g. alterar senha do usuário
+** sempre que utilizar os métodos, vamos usar a mesma estrutura - rota e parametros
+** Posso usar a mesma rota no GET e POST? sim, pq estamos usando métodos diferentes.
+*/
+
+app.get("/", (request, response) => {
+    return response.send("Olá NLW 05");
+});
+
+app.post("/", (request, response) => {
+    return response.json({ message: "usuário salvo com sucesso!"})
+});
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
