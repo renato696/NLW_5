e333d13d0166781037af5d9285052217986d4de5
Renato Pires <renato696@gmail.com>
2021-04-23 07:07:41 -0300

    Criação de rotas
    Criação do arquivo routes.ts e colocando ele no server.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,38 @@
 import { Router } from "express";
 
+import { getCustomRepository } from "typeorm";
+
+import { SettingsRepository } from "./repositories/SettingsRepository";
+
 const routes = Router();
+
+/**
+ * Tipos de parametros:
+ * Routes Params = parametros de rotas
+ * e.g. https//localhost:333/settings/1
+ * 
+ * Query Params = parametros de busca
+ * e.g. https://localhost:333/settings/1?search=algumaCoisa
+ * 
+ * Body Params = passa objetos/json dentro das requisições
+ * e.g. {
+ *  }
+ */
+
+
+routes.post("/settings", async (request, response) => {
+    const {chat, username } = request.body;
+    
+    const settingsRepository = getCustomRepository(SettingsRepository);
+
+    const settings = settingsRepository.create({
+        chat,
+        username
+    })
+
+    await settingsRepository.save(settings);
+
+    return response.json(settings);
+})
 
 export { routes };
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
 import express, { response } from "express";
 
 import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.send("Olá NLW 05");
-});
-
-app.post("/", (request, response) => {
-    return response.json({ message: "usuário salvo com sucesso!"})
-});
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
