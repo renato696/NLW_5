d06fc0c5f7a3ee999bb97b959fabece496f161de
Renato Pires <renato696@gmail.com>
2021-04-23 06:49:04 -0300

    Criação do Routes.ts
    Cria arquivo de rotas

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,6 @@ import "./database";
 
 const app = express();
 
-/*
-* GET = busca
-* POST = criação
-* PUT = alteração
-* DELETE = deletar
-* PATCH = alterar alguma informação específica - e.g. alterar senha do usuário
-** sempre que utilizar os métodos, vamos usar a mesma estrutura - rota e parametros
-** Posso usar a mesma rota no GET e POST? sim, pq estamos usando métodos diferentes.
-*/
-
 app.get("/", (request, response) => {
     return response.send("Olá NLW 05");
 });
