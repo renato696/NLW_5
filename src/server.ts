f02da7b213b8743ab104bb030f4792ed6cf5a0d2
Renato Pires <renato696@gmail.com>
2021-04-24 17:41:05 -0300

    Update server.ts
    fazendo com que o express utilize a pasta public

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express, { response } from "express";
 import { createServer } from "http";
 import { Server, Socket} from "socket.io";
+import path from "path";
 
 import "./database";
 import { routes } from "./routes";
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 const http = createServer(app); //criando o protocolo http
 const io = new Server(http); //criando o protocolo WS
