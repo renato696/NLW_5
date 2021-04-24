1fdf06d6bd70b314fe7c747c5fd3022b9e39df29
Renato Pires <renato696@gmail.com>
2021-04-23 22:16:04 -0300

    Correção no UserController
    correção no const userController

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -15,4 +15,4 @@ class UsersController {
 
 }
 
-export { UsersController }
+export { UsersController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { SettingsController } from "./controllers/SettingsController";
+import { UsersController } from "./controllers/UsersController";
+import { UsersRepository } from "./repositories/UsersRepository";
 
 const routes = Router();
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
 
 routes.post("/settings", settingsController.create);
+routes.post("/users", usersController.create);
 
 export { routes };
 
