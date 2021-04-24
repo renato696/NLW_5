c7716ba412803e7701adc41a91f150cd518cdecf
Renato Pires <renato696@gmail.com>
2021-04-24 20:23:52 -0300

    correções
    correções no ConnectionsRepository e ConnectionsService

--- src/repositories/ConnectionsRepository.ts
+++ src/repositories/ConnectionsRepository.ts
@@ -3,9 +3,9 @@ import { Connection } from "../entities/Connection";
 //pois o typeorm tambem tem um connections, então temos que garantir que este nosso vem do entities.
 
 
-class ConnectionRepository extends Repository<Connection> {
+class ConnectionsRepository extends Repository<Connection> {
 
 
 }
 
-export { ConnectionRepository };
+export { ConnectionsRepository };
---
+++ src/services/ConnectionsService.ts
@@ -0,0 +1,10 @@
+
+import { getCustomRepository } from "typeorm";
+import { ConnectionsRepository } from "../repositories/ConnectionsRepository";
+
+class ConnectionsService {
+
+
+}
+
+export { ConnectionsService };
