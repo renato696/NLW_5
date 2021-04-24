c6ba17e5bda221d74e5c652e4232461825969bf2
Renato Pires <renato696@gmail.com>
2021-04-24 19:45:55 -0300

    Criação das colunas da tabela
    criação das colunas das tabelas

--- src/database/migrations/1619303689791-CreateConnections.ts
+++ src/database/migrations/1619303689791-CreateConnections.ts
@@ -1,11 +1,47 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateConnections1619303689791 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "connections",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                    }, 
+                    {
+                        name: "admin_id",
+                        type: "uuid",
+                        isNullable: true,
+                    },
+                    {
+                        name: "user_id",
+                        type: "uuid",
+                    },
+                    {
+                        name: "socket_id",
+                        type: "varchar" //pois não sabemos qual é o tipo
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("connections")
     }
 
 }
