3e8ebf1ff8b3780f077716660ecb84302fa147b6
Renato Pires <renato696@gmail.com>
2021-04-24 10:07:45 -0300

    Update no entities/message.ts
    bug corrigido no message.ts, nome da tabela.

--- src/entities/Message.ts
+++ src/entities/Message.ts
@@ -2,7 +2,7 @@ import { Entity, PrimaryColumn, CreateDateColumn, Column, ManyToOne, JoinColumn 
 import { v4 as uuid } from "uuid";
 import { User } from "./User";
 
-@Entity("message")
+@Entity("messages")
 class Message {
 
     @PrimaryColumn()
