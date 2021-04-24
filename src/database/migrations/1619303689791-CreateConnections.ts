5145e2b9a02355ad2317b5e5847a0365520b21f0
Renato Pires <renato696@gmail.com>
2021-04-24 19:53:59 -0300

    Criação da ForeignKey
    criação da foreignkey

--- src/database/migrations/1619303689791-CreateConnections.ts
+++ src/database/migrations/1619303689791-CreateConnections.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
 
 export class CreateConnections1619303689791 implements MigrationInterface {
 
@@ -38,9 +38,23 @@ export class CreateConnections1619303689791 implements MigrationInterface {
                 ]
             })
         );
+
+        await queryRunner.createForeignKey(
+            "connections",
+            new TableForeignKey({
+                name: "FKConnectionUser",
+                referencedTableName: "users",
+                referencedColumnNames: ["id"],
+                columnNames: ["user_id"],
+                onDelete: "SET NULL",
+                onUpdate: "SET NULL"
+            })
+        );
+
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey("connections", "FKConnectionUser");
         await queryRunner.dropTable("connections")
     }
 
