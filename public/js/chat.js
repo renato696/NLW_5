7b77ba1840755e49bfc6a982cd3bf85fb359be7e
Renato Pires <renato696@gmail.com>
2021-04-24 19:30:38 -0300

    Update chat.js
    ajustes

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,7 +8,6 @@ document.querySelector("#start_chat").addEventListener("click", (event) => {
 
     const chat_in_support = document.getElementById("chat_in_support");
     chat_in_support.style.display = "block";
-    
 
     const email = document.getElementById("email").value;
     const text = document.getElementById("txt_help").value;
@@ -17,7 +16,7 @@ document.querySelector("#start_chat").addEventListener("click", (event) => {
         const params = {
             email,
             text,
-        }
+        };
         socket.emit("client_first_access", params, (call, err) => {
             if(err) {
                 console.err(err)
