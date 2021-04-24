336d89847e083878d1a371e831e96dda15cb98b6
Renato Pires <renato696@gmail.com>
2021-04-24 17:28:23 -0300

    Update server.ts
    importação do protocolo http e ws, e criação do protocolo http e ws

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express, { response } from "express";
+import { createServer } from "http";
+import { Server, Socket} from "socket.io";
 
 import "./database";
 import { routes } from "./routes";
 
 const app = express();
+
+const http = createServer(app); //criando o protocolo http
+const io = new Server(http); //criando o protocolo WS
 
 app.use(express.json())
 
