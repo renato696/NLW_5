a56f701d68c37955c8bbc9eb47a97e8439ad765d
Renato Pires <renato696@gmail.com>
2021-04-24 19:14:02 -0300

    Update chat.js
    fazer com que o chat.js pegue o que foi digitado no chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,4 +2,14 @@ document.querySelector("#start_chat").addEventListener("click", (event) => {
     console.log("Cliquou no Botão!")
 
     const socket = io();
+
+    const chat_help = document.getElementById("chat+help");
+    chat_help.style.display = "none";
+
+    const chat_in_support = document.getElementById("chat_in_support");
+    chat_in_support.style.display = "block";
+
+    const email = document.getElementById("email").value;
+    const text = document.getElementById("txt_help").value;
+    
 });
