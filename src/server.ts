fa67a508099755afaf70db7542adc2ae50a755aa
Renato Pires <renato696@gmail.com>
2021-04-24 17:44:51 -0300

    Update server.ts
    alterações para que o node consiga entender o html, pois, por padrão, ele usa ejs.

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,9 @@ import { routes } from "./routes";
 const app = express();
 
 app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(__dirname, "..", "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
 const http = createServer(app); //criando o protocolo http
 const io = new Server(http); //criando o protocolo WS
