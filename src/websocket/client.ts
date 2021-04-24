dcfae03dfa4fec21f7f4a0ce84507c57dde8c91d
Renato Pires <renato696@gmail.com>
2021-04-24 19:54:20 -0300

    Update client.ts
    anotação

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -3,5 +3,7 @@ import { io } from "../http";
 io.on("connect", (socket) => {
     socket.on("client_first_access", (params) => {
         console.log(params);
+
+        //salvar a conexao com o socket_id, user_id,
     });
 });
