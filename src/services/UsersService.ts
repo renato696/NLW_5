9ef17499eba05cb256ebe784ee82cd2dcdfa8a31
Renato Pires <renato696@gmail.com>
2021-04-24 21:05:35 -0300

    Ajuste no UsersService e finalização do client
    ajuste necessário no UsersServices, e finalização do client. Agora conseguimos salvar as informações.

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -30,6 +30,11 @@ class UsersService {
         
     }
 
+    async findByEmail(email: string) {
+        const user = await this.usersRepository.findOne({ email});
+        
+        return user;
+    }
 }
 
 export { UsersService }
