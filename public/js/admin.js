f2f9a155d02e872cbcd97c48bafca74689604fac
Renato Pires <renato696@gmail.com>
2021-04-25 17:23:55 -0300

    resolvendo bugs
    resolvendo o problema das mensagens do client nao enviadas

--- public/js/admin.js
+++ public/js/admin.js
@@ -79,7 +79,7 @@ function sendMessage(id) {
   const createDiv = document.createElement("div");
 
   createDiv.className = "admin_message_admin";
-  createDiv.innerHTML = `Atendente: <spam>${params.text}</spam>`;
+  createDiv.innerHTML = `Atendente: <span>${params.text}</span>`;
   createDiv.innerHTML += `<spam class="admin_date">${dayjs().format("DD/MM/YYYY HH:mm:ss")}`;
 
   divMessages.appendChild(createDiv);
@@ -92,13 +92,13 @@ socket.on("admin_receive_message", (data) => {
   const divMessages = document.getElementById(`allMessages${connection.user_id}`);
 
   const createDiv = document.createElement("div");
-  
+
   createDiv.className = "admin_message_client";
 
-  createDiv.innerHTML = `<spam>${connection.user.email} </spam>`;
-  createDiv.innerHTML += `<spam>${params.message.text}</spam>`;
+  createDiv.innerHTML = `<span>${connection.user.email} </span>`;
+  createDiv.innerHTML += `<span>${params.message.text}</span>`;
 
-  createDiv.innerHTML += `<spam class="admin_date">${dayjs(params.message.created_at).format("DD/MM/YYYY HH:mm:ss")}</spam>`;
+  createDiv.innerHTML += `<span class="admin_date">${dayjs(params.message.created_at).format("DD/MM/YYYY HH:mm:ss")}</span>`;
 
   divMessages.appendChild(createDiv);
 });
