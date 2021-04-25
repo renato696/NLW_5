adbfb2ef87f1db336e5bcd3b3a39120b9bc9884b
Renato Pires <renato696@gmail.com>
2021-04-25 00:04:54 -0300

    Update routes.ts
    acesso a nova rota findByUserName

--- src/routes.ts
+++ src/routes.ts
@@ -11,6 +11,8 @@ const usersController = new UsersController();
 const messagesController = new MessagesController();
 
 routes.post("/settings", settingsController.create);
+routes.post("/settings/:username", settingsController.findByUserName);
+
 
 routes.post("/users", usersController.create);
 
