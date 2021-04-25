a359338b1ff7237436fc27caddc70eca0429c752
Renato Pires <renato696@gmail.com>
2021-04-25 12:14:41 -0300

    cria lista dos clients sem admin
    cria a listagem dos clients sem admin

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -39,6 +39,16 @@ class ConnectionsService {
 
         return connection;
     }
+
+    async findAllWithoutAdmin() {
+        const connections = await this.connectionsRepository.find({
+            where: { admin_id: null},
+            relations: ["user"]
+        });
+
+        return connections;
+    }
+
 }
 
 export { ConnectionsService };
