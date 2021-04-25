d7770fac24cfef06925be3f62cb29f050e17f810
Renato Pires <renato696@gmail.com>
2021-04-24 21:18:59 -0300

    Update ConnectionsRepository.ts
    correção no ConnectionsRepository

--- src/repositories/ConnectionsRepository.ts
+++ src/repositories/ConnectionsRepository.ts
@@ -1,10 +1,9 @@
-import { Repository } from "typeorm";
+import { EntityRepository, Repository } from "typeorm";
 import { Connection } from "../entities/Connection"; 
 //pois o typeorm tambem tem um connections, então temos que garantir que este nosso vem do entities.
 
-
+@EntityRepository(Connection)
 class ConnectionsRepository extends Repository<Connection> {
-
 
 }
 
